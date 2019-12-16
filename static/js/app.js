d622bb45a12ce49c89aaa6cff70a9bc0ced31b88
Chinchinberry <jonjon0424@gmail.com>
2019-12-16 00:13:18 -0800

    working on py to js

--- static/js/app.js
+++ static/js/app.js
@@ -15,12 +15,39 @@ function loadJsonData() {
 
 function loadCsvData(){
 
-  d3.csv("/data_csv", function(data) {
-    console.log(data['name']);
-  });
+  // d3.csv("/data_csv", function(data) {
+  //   console.log(data['name']);
+  // });
 
 }
+
+function loadMap(){
+  console.log("loading map");
+
+  // Creating our initial map object
+  // We set the longitude, latitude, and the starting zoom level
+  // This gets inserted into the div with an id of 'map'
+  var myMap = L.map("map", {
+    center: [45.52, -122.67],
+    zoom: 13
+  });
+
+  // Adding a tile layer (the background map image) to our map
+  // We use the addTo method to add objects to our map
+  // L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  //   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  //   maxZoom: 18,
+  //   id: "mapbox.streets",
+  //   accessToken: "pk.eyJ1IjoicGd1aWxsZXJnYW4iLCJhIjoiY2szcDF2dG5zMjdjajNua29zdzBzNDV3NCJ9.en3jyfbLQe5qMhWKW-NZmA"
+  // }).addTo(myMap);
+  var basemap = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+  });
+
+  basemap.addTo(myMap);
+};
 
 
 loadJsonData();
 loadCsvData();
+loadMap()
---
+++ static/js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoicGd1aWxsZXJnYW4iLCJhIjoiY2szcDF2dG5zMjdjajNua29zdzBzNDV3NCJ9.en3jyfbLQe5qMhWKW-NZmA";
